1665de50851813b85fe84edca7fc8c38bfa9c4d6
Rainbowshine295 <79471824+Rainbowshine295@users.noreply.github.com>
2021-09-01 19:41:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
-status = " ";
+status = "";
+objects = [];
 
 function preload() {
     img = loadImage('dog_cat.jpg');
@@ -24,19 +25,26 @@ if (error) {
     console.log(error);
 }
     console.log(results);
+
+    objects = results;
+
 }
 
 function draw() {
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog" , 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
 
-    fill("#4ff07d");
-    text("Cat" , 320, 120);
-    noFill();
-    stroke("#4ff07d");
-    rect(300, 90, 270, 320);
+    if (status != "") {
+       
+        for (i = 0 ; i< objects.length ; i++) {
+            
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            fill("#FF0000");
+            percentage = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percentage + "%" , objects[i].x , objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height);
+
+        }
+    }
 }
